59234977ad2fc69ee6dcc2a754cd8995b28d2fcf
DevGoodlink <goodlink.fes@gmail.com>
2019-03-03 19:50:17 +0100

    Update js.js

--- js.js
+++ js.js
@@ -1,13 +1,3 @@
-// Etudier l'exemple ci-dessous qui affiche la liste des contacts
-// Ajouter dans le tableau le nom de la société et son type à laquelle appartient chaque contact
-// Ajouter un champ texte au dessus du tableau pour filtrer le tableau par (nom, prenom, ou nom de son organisation)
-// Ajouter un formulaire en dessous du tableau pour la modification d'un contact a la selection d'une ligne du tableau
-// Ajouter un bouton au dessus du formulaire pour ajouter un contact
-// AJouter un bouton a coté du precedent avec un mini formulaire pour ajouter un organisation
-// Ajouter un loader lors du chargement / mise a jour du tableau (qui s'affiche lors de l'appel GetContacts et qui se cache lord de l'appel updateContactsTable)
-
-
-
 var contacts = [];
 var organisations=[];
 var fullcontact=[];
@@ -26,16 +16,14 @@ function init(){
             fullcontact.push(elt);
         });
         updateContactsTable(fullcontact);
-        
-        
-        
-        
-        
+       
     });
      
 }
+
 function updateSelectOrganisations(){
     var x = document.getElementById("organisation");
+	
     organisations.forEach(function(elt){
         var option = document.createElement("option");
         option.text = elt.nom;
@@ -44,35 +32,54 @@ function updateSelectOrganisations(){
     });
 }
 
-
+// J'ai utilisé cette fontion pour la modification d'un contact a la selection d'une ligne du tableau
 function changeValue(){
-    //console.log("change detected");
+    
     var id = parseInt(document.querySelector("#idcontact").value);
-    var contact = fullcontact.filter(c => c.id===id).pop();
-
-    contact.nom=document.querySelector("#nom").value;
-    contact.prenom =document.querySelector("#prenom").value;
-    contact.orgnisationid  = document.querySelector("#organisation").value;
-    contact.NomOrganisation=document.querySelector("#organisation").options[document.querySelector("#organisation").selectedIndex].text;
-    contact.civilite = document.querySelector("#civilite").value;
-    updateContactsTable(fullcontact);
+	if(id){
+		var contact = fullcontact.filter(c => c.id===id).pop();
+		contact.nom=document.querySelector("#nom").value;
+		contact.prenom =document.querySelector("#prenom").value;
+		contact.orgnisationid  = parseInt(document.querySelector("#organisation").value);
+		contact.NomOrganisation=document.querySelector("#organisation").options[document.querySelector("#organisation").selectedIndex].text;
+		contact.TypeOrganisation=organisations.filter(o => o.id==contact.orgnisationid).pop().id;
+		contact.civilite = document.querySelector("#civilite").value;
+		updateContactsTable(fullcontact);
+	}
+    
     
 }
-function AjouterOrganisation(){
+function ajouterOrganisation(){
+
     var organisation = {
-        id:document.getElementById("idorganisation").value ,
+        id:parseInt(document.getElementById("idorganisation").value),
         nom: document.getElementById("nomOrganisation").value ,
-        nbemployes: document.getElementById("nbEmploye").value ,
+        nbemployes: parseInt(document.getElementById("nbEmploye").value) ,
         type: document.getElementById("type").value
     };
     organisations.push(organisation);
-
+	alert("Organisation a été ajoutée avec succées");
+	document.getElementById("idorganisation").value="";
+	document.getElementById("nomOrganisation").value="" ;
+    document.getElementById("nbEmploye").value="" ;
+    document.getElementById("type").value="";
+	
 
 }
-function AjouterContact(){
 
-
+function ajouterContact(){
+	var contact={};
+	contact.id = fullcontact.length+1;
+	contact.nom= document.querySelector("#nom").value;
+	contact.prenom= document.querySelector("#prenom").value;
+	contact.orgnisationid  = document.querySelector("#organisation").value;
+	contact.NomOrganisation=document.querySelector("#organisation").options[document.querySelector("#organisation").selectedIndex].text;
+	contact.TypeOrganisation=organisations.filter(o => o.id==contact.orgnisationid).pop().type;
+	contact.civilite= document.querySelector("#civilite").value;
+	fullcontact.push(contact);
+	updateContactsTable(fullcontact);
 }
+
 function afficheDansFormulaire(id) {
     var contact = contacts.filter(c => c.id==id).pop();
     document.querySelector("#idcontact").value=id;
@@ -81,10 +88,12 @@ function afficheDansFormulaire(id) {
     document.querySelector("#organisation").value=contact.orgnisationid;
     document.querySelector("#civilite").value=contact.civilite;
 }
+//La fonction qui permet de parcourir le tableau
 function filterTableau(){
     var word = document.getElementById("filterword").value;
-    if(word.length>0){
-        newtab = fullcontact.filter((c)=>{
+    var filtredData=[];
+	if(word.length>0){
+        filtredData = fullcontact.filter((c)=>{
             var values = Object.values(c);
             var flag = false;
             values.forEach((val) => {
@@ -98,7 +107,7 @@ function filterTableau(){
             if(flag) return c;
         }) ;
             
-        updateContactsTable(newtab);
+        updateContactsTable(filtredData);
     }else{
         updateContactsTable(fullcontact);
     }
@@ -116,24 +125,28 @@ function updateContactsTable(contacts){
         html += "<td>" + contacts[i].TypeOrganisation+ "</td>";
         html += "</tr>";
     }
+	
     html +="<tr>";
-    html +="<td><select id='civilite' onchange='changeValue()'><option value='Mr'>Mr</option><option value='Mme'>Mme</option></select></td>";
-    html +="<td><input type='text' id='nom' onchange='changeValue()'/></td>";
-    html +="<td><input type='text' id='prenom' onchange='changeValue()'/></td>";
-    html +="<td><select id='organisation' onchange='changeValue()'></select></td>";
-    html +="<td><input type='text' id='idcontact' hidden/></td>";
+    html +="<td><select id='civilite' onchange='changeValue()' class='custom-select mr-sm-2'><option value='Mr'>Mr</option><option value='Mme'>Mme</option></select></td>";
+    html +="<td><input type='text' id='nom' onchange='changeValue()' class='form-control mr-sm-2'/></td>";
+    html +="<td><input type='text' id='prenom' onchange='changeValue()' class='form-control mr-sm-2'/></td>";
+    html +="<td><select id='organisation' onchange='changeValue()' class='custom-select mr-sm-2'></select></td>";
+    html +="<td><input type='text' id='idcontact' hidden/><button onclick='ajouterContact();' class='btn btn-warning'>Ajouter contact</button></td>";
     html +="</tr>";
-    
+
     $("#tableContacts").html(html);
     updateSelectOrganisations();
 }
 
 //Recuperer la liste de contacts
-function GetContacts(callback){
+function GetContacts(callback){ 
+    $(".spinner").show();
     setTimeout(function(){
         callback(JSON.parse(localStorage.contacts));
+		 $('.spinner').hide();
     }, 1000);
 }
+//Recuperer le nom et le type d'organisations
 function GetOrganizations(callback){
     setTimeout(function(){
         callback(JSON.parse(localStorage.organisations));
@@ -166,4 +179,3 @@ function initStorage(){
     }
 
 }
-
